a4d0a94a4bbf0283192645dfe10f21ab003c9a76
Daniel Joseph Van Twisk <vantwisk@r610-2-0.local>
2022-03-28 14:16:53 -0400

    longgf_helper

--- longgf_helper.sh
+++ longgf_helper.sh
@@ -5,8 +5,11 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --mem-per-cpu 10g
 
+MINIMAP_DIR=${7}
+LONGGF_DIR=${8}
+
 #singularity exec --pid --bind /datastore longgf_0.1.2--h05f6578_1.sif \
-  ~/LongGF/bin/LongGF \
-  fun-med/fuse-${1}-${4}-sorted_minimap2-splice.bam \
+~/LongGF/bin/LongGF \
+  ${MINIMAP_DIR}/fusions-${1}-${5}-${6}-${4}-sorted-n.bam \
   Homo_sapiens.GRCh38.105.gtf \
-  40 50 100 > fun-med/fuse-${1}-${4}-sorted_minimap2-splice.log
+  40 50 100 > ${LONGGF_DIR}/fusions-${1}-${5}-${6}-${4}.log
