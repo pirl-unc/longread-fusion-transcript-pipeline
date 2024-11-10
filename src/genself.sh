752e8d1b4148b016a68730d4719497033bb5cb60
dvantwisk <dvantwisk@gmail.com>
2024-11-09 21:55:10 -0500

    Updated Dockerfile

--- src/genself.sh
+++ src/genself.sh
@@ -5,6 +5,6 @@
 #SBATCH -n 1
 #SBATCH -t 12:00:00
 
-/home/vantwisk/minimap2-2.21_x64-linux/minimap2 $CDNA_REFERENCE $CDNA_REFERENCE -X -t $THREADS -2 -c -o $REF_STORAGE_DIR/genion-selfalign.paf
+/home/vantwisk/minimap2-2.21_x64-linux/minimap2 $CDNA_REFERENCE_ENS $CDNA_REFERENCE_ENS -X -t $THREADS -2 -c -o $REF_STORAGE_DIR/genion-selfalign.paf
 cat $REF_STORAGE_DIR/genion-selfalign.paf | cut -f1,6 | sed 's/_/\t/g' | awk 'BEGIN{OFS="\t";}{print substr($1,1,15),substr($2,1,15),substr($3,1,15),substr($4,1,15);}' | awk '$1!=$3' | sort | uniq > $GENSELF
 
